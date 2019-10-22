8297f9322731d62362e954ddbd521e11d663856e
Marc <marcdwest@gmail.com>
2019-10-22 10:38:21 -0500

    ngStyle binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,10 +5,18 @@ import { Component } from '@angular/core';
 	templateUrl: './server.component.html',
 })
 export class ServerComponent {
-	serverID: number = 10;
-	serverStatus: string = 'offline';
+	serverID = 10;
+	serverStatus = 'offline';
 
-	getServerStatus(){
+	constructor() {
+		this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+	}
+
+	getServerStatus() {
 		return this.serverStatus;
+	}
+
+	getColor() {
+		return this.serverStatus === 'online' ? 'lightGreen' : 'mistyRose';
 	}
 }
